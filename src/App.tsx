adb52b51192fc6de2695983e497219f3ec7b8bf1
Rohit Pal <rohit.coding.0123@gmail.com>
2023-04-09 14:17:36 +0530

    Added Header, Footer and TextInput components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TextInput from "./components/TextInput";
 
 const App = () => {
   return (
     <>
       <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
         <Container maxW="3xl" centerContent>
-          <h1>My project</h1>
+          <Header />
+          <Footer />
+          <TextInput />
         </Container>
       </Box>
     </>
